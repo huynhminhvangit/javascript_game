52fb5b8f4c7428c051db3753571f9f7344e57dc6
Vang Huynh <vanghm@imt-soft.com>
2022-10-09 09:59:13 +0700

    sprite animation

--- script.js
+++ script.js
@@ -56,9 +56,13 @@ window.addEventListener('load', function () {
       this.height = 190;
       this.x = 20;
       this.y = 100;
+      this.frameX = 0;
+      this.frameY = 0;
+      this.maxFrame = 37;
       this.speedY = 0;
       this.maxSpeed = 3;
       this.projectiles = [];
+      this.image = document.getElementById('player');
     }
 
     update () {
@@ -72,11 +76,20 @@ window.addEventListener('load', function () {
       });
 
       this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
+      // sprite animation
+      if(this.frameX < this.maxFrame) {
+        this.frameX++;
+      } else {
+        this.frameX = 0;
+      }
+
+
     }
 
     draw (context) {
       context.fillStyle = 'black';
       context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
       // handle projectiles
       this.projectiles.forEach(projectile => {
         projectile.draw(context);
@@ -163,13 +176,13 @@ window.addEventListener('load', function () {
       this.layers.push(this.layer3);
     }
 
-    update() {
+    update () {
       this.layers.forEach(layer => {
         layer.update();
       });
     }
 
-    draw(context) {
+    draw (context) {
       this.layers.forEach(layer => {
         layer.draw(context);
       });
